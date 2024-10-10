9021eceda448c1b004826ba291f0e7cae0191326
CamilaLoli <u202110385@upc.edu.pe>
2024-10-09 19:41:09 -0500

    Agregar archivos de proyecto.

---
+++ CatchUpPlatform.API/News/Domain/Services/IFavoriteSourceQueryService.cs
@@ -0,0 +1,9 @@
+﻿using CatchUpPlatform.API.News.Domain.Model.Aggregates;
+
+namespace CatchUpPlatform.API.News.Domain.Services
+{
+    public interface IFavoriteSourceQueryService
+    {
+        Task<IFavoriteSource?> Handle(GetFavoriteSourceByIdQuery)
+    }
+}
